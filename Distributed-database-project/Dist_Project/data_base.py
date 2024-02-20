4238362a2d4068b63cc8306e4521e3a93b893d7c
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-02-20 17:34:30 +0200

    student assistant: the mobile app and distributed database project

---
+++ Distributed-database-project/Dist_Project/data_base.py
@@ -0,0 +1,231 @@
+# pip install sqlalchemy
+# pip install pyodbc
+# pip install os
+from sqlalchemy import create_engine, text
+import pandas as pd
+import os
+from datetime import datetime
+
+"""
+A class for us to use to access the database
+"""
+class data_base():
+    servers = {
+        'cairo': '20.25.37.239',
+        'alex': '172.203.249.249',
+        'psaid': '20.216.4.130'
+    }
+    username = "sa"
+    password = "SQLServer123"
+    driver = "ODBC Driver 17 for SQL Server"
+    conn_strs = {
+        'cairo': f"mssql+pyodbc://{username}:{password}@{servers['cairo']}/{'cairo'}?driver={driver}",
+        'alex_replication': f"mssql+pyodbc://{username}:{password}@{servers['cairo']}/{'alex_replication'}?driver={driver}",
+        'psaid': f"mssql+pyodbc://{username}:{password}@{servers['psaid']}/{'portsaid'}?driver={driver}",
+        'cairo_replication': f"mssql+pyodbc://{username}:{password}@{servers['psaid']}/{'cairo_replication'}?driver={driver}",
+        'alex': f"mssql+pyodbc://{username}:{password}@{servers['alex']}/{'Alexandria'}?driver={driver}",
+        'port_replication': f"mssql+pyodbc://{username}:{password}@{servers['alex']}/{'port_replication'}?driver={driver}",
+    }
+
+    """
+    Server : Alex => port_replication
+    Server : Cairo => alex_replication
+    Server : Psaid => cairo_replication
+    """
+
+    """Uncomment if all the servers are running
+    as it will give an error and the code won't
+    run if else"""
+    c_engine = create_engine(conn_strs['cairo'])
+    c_con = c_engine.connect()
+    # a_engine = create_engine(conn_strs['alex'])
+    # a_con = a_engine.connect()
+    # p_engine = create_engine(conn_strs['psaid'])
+    # p_con = p_engine.connect()
+    # c_r_engine = create_engine(conn_strs['cairo_replication'])
+    # c_r_con = c_r_engine.connect()
+    a_r_engine = create_engine(conn_strs['alex_replication'])
+    a_r_con = a_r_engine.connect()
+    # p_r_engine = create_engine(conn_strs['port_replication'])
+    # p_r_con = p_r_engine.connect()
+
+    connections = {
+        "cairo": c_con,
+        # "alex": a_con,
+        # "psaid": p_con
+    }
+
+    """The connection function to connect to Cairo
+    server and database"""
+    def cairo_update(self, pid, count):
+        self.update(pid, count, self.c_con)
+        # self.update(pid, count, self.c_r_con)
+
+    """The connection function to connect to Port-Said
+    server and database"""
+    def psaid_update(self, pid, count):
+        self.update(pid, count, self.p_con)
+        self.update(pid, count, self.p_r_con)
+
+    """The connection function to connect to Alexandria
+    server and database"""
+    def alex_update(self, pid, count):
+        self.update(pid, count, self.a_con)
+        self.update(pid, count, self.a_r_con)
+
+
+    """General updating method to be used for all
+    servers and databases"""
+    def update(self, pid, count, connection):
+        pid = int(pid)
+        con = connection
+        count = self.validate_count(pid, count, con)
+        if type(count) != int:
+            print("can't remove number higher than original number")
+            return
+        current_datetime = datetime.now().isoformat()
+
+        if count < 0:
+            con.execute(text(f"update inventory set quantity -= {abs(count)} where pid = {pid}"))
+            con.execute(text(f"insert into transactions (pid, quantity, p_state,p_timestamp) values ({pid}, {abs(count)}, 0, '{current_datetime}')"))
+        elif count > 0:
+            con.execute(text(f"update inventory set quantity += {abs(count)} where pid = {pid}"))
+            con.execute(text(f"insert into transactions (pid, quantity, p_state,p_timestamp) values ({pid}, {abs(count)}, 1, '{current_datetime}')"))
+        con.commit()
+
+    """The validation for the count in order not to
+    remove number more than the number in inventory"""
+    def validate_count(sef, id, val, con):
+        if type(val).__name__ != 'int':
+            val = int(val)
+        rs = con.execute(text(f'select quantity from inventory where pid={id}'))
+        results = rs.fetchall()
+        if val < 0:
+            if abs(val) > results[0][0]:
+                return ""
+        return val
+
+    """This will look in what server to update"""
+    def up(self, pid, count):
+        if os.environ['server'] == 'cairo':
+            self.cairo_update(pid, count)
+        elif os.environ['server'] == 'alex':
+            self.alex_update(pid, count)
+        elif os.environ['server'] == 'psaid':
+            self.psaid_update(pid, count)
+
+    # def query(self , cities , products_id):
+    #     results_cairo=[]
+
+    #     results_alex=[]
+
+    #     results_psaid=[]
+
+    #     all_results=[]
+
+    #     products_id=tuple(products_id)
+    #     query=text(f'''
+    #         SELECT p_name, sex, quantity
+    #         FROM products
+    #         JOIN inventory ON products.ID = inventory.pid
+    #         WHERE products.ID IN {products_id}
+    #     ''')
+    #     if len(products_id) ==1:
+    #         products_id=products_id[0]
+    #         query=text(f'''
+    #         SELECT p_name, sex, quantity
+    #         FROM products
+    #         JOIN inventory ON products.ID = inventory.pid
+    #         WHERE products.ID = {products_id}
+    #     ''')
+        
+        
+    #     products_id=tuple(products_id)
+    #     if "psaid" in cities:
+    #         result=self.p_con.execute(query)
+    #         results_psaid.append(result.fetchall())
+    #         print(results_psaid) 
+    #         all_results.append(results_psaid)
+    #     if "cairo" in cities:
+    #         result=self.c_con.execute(query)
+    #         results_cairo.append(result.fetchall())
+    #         print(results_cairo)
+    #         all_results.append(results_cairo) 
+    #     if "alex" in cities:
+    #         result=self.a_con.execute(query)
+    #         results_alex.append(result.fetchall())
+    #         all_results.append(results_alex)
+    #     return all_results
+
+    def query(self , cities , products_id):
+        all_items = self.connections['cairo'].execute(text(f"select id, p_name from products"))
+        all_items = all_items.fetchall()
+        items_dict = {}
+        for i in all_items:
+            if i[0] not in items_dict.keys():
+                items_dict[i[0]] = i[1]
+        result1 = []
+        for j in products_id:
+            result2 = []
+            total = 0
+            for i in cities:
+                result3 = []
+                rs = self.connections[i].execute(text(f"select * from inventory where pid={j}"))
+                results = rs.fetchall()
+                result3.append(items_dict[results[0][0]])
+                result3.append(i)
+                result3.append(results[0][1])
+                result2.append(result3)
+                total += results[0][1]
+            result2.append(['total', total])
+            result1.append(result2)
+        return result1
+
+    """This will look in what server to update"""
+    def up(self, pid, count):
+        if os.environ['server'] == 'cairo':
+            self.cairo_update(pid, count)
+        elif os.environ['server'] == 'alex':
+            self.alex_update(pid, count)
+        elif os.environ['server'] == 'psaid':
+            self.psaid_update(pid, count)
+
+    """This will look in what server to get transaction"""
+
+    def get_transactions(self):
+        server = os.environ['server']
+
+        if server == 'cairo':
+            return self.c_get_transactions()
+        elif server == 'alex':
+            return self.a_get_transactions()
+        elif server == 'psaid':
+            return self.p_get_transactions()
+
+    """The connection function to connect to Cairo
+    server and database"""
+
+    def c_get_transactions(self):
+        return self.get_transactions_from_db(self.c_con)
+
+    """The connection function to connect to Alexandria
+    server and database"""
+
+    def a_get_transactions(self):
+        return self.get_transactions_from_db(self.a_con)
+
+    """The connection function to connect to Port-Said
+    server and database"""
+
+    def p_get_transactions(self):
+        return self.get_transactions_from_db(self.p_con)
+
+    """The function to get data from transactions"""
+
+    def get_transactions_from_db(self, connection):
+        # Execute SQL query to select transactions
+        result = connection.execute(text('SELECT  p_name,p_state,quantity,p_timestamp FROM transactions JOIN products ON pid = ID ORDER BY p_timestamp desc;'))
+        transactions = result.fetchall()
+        return transactions
+
+    
---
+++ Distributed-database-project/Dist_Project/flask_and_sqlalchemy_script.py
@@ -0,0 +1,64 @@
+"""
+To use the code run this file
+"""
+# pip install flask
+from flask import Flask, render_template, request
+from flask import render_template
+from __init__ import db
+import os
+
+app = Flask(__name__)
+
+"""
+the @app.route(...) is used to move the user from one
+page to another.
+render_template("*.html") is for serving the html page
+to the browser.
+the POST and GET are for regalting the reading and writing
+to and from the web page.
+"""
+@app.route("/about")
+def hello():
+    return render_template("about.html")
+
+@app.route("/search", methods=['GET', 'POST'])
+def search():
+    if request.method == 'POST':
+        cities=request.form.getlist('city')
+        products=request.form.getlist("product")
+        all_results = db.query(cities,products)
+        return render_template("search.html", items=all_results)
+    else:
+        return render_template("search.html")
+
+
+
+@app.route("/", methods = ['GET', 'POST'])
+def account():
+    data = request.form.get('server')
+    password = request.form.get('password')
+    os.environ['server'] = str(data)
+    if str(password)==str(data)+'123':
+        return render_template("products.html")
+    else:
+        return render_template("index.html")
+
+@app.route("/cart")
+def cart():
+    transactions = db.get_transactions()
+    return render_template("cart.html",transactions=transactions)
+
+@app.route("/products")
+def products():
+    return render_template("products.html")
+
+@app.route("/products-details", methods=['GET', 'POST'])
+def products_details():
+    if request.method == 'POST':
+        pid = request.form.get('pid')
+        count = request.form.get('count')
+        db.up(pid, count)
+    return render_template("products-details.html")
+
+if __name__ == "__main__":
+    app.run()
